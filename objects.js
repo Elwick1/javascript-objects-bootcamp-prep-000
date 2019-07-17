00ceca9e28a0ce8671620a38e2fc8d84f5883451
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-17 12:36:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {"My Bloody Valentine":"Sometimes","Slowdive":"Alison"}
 
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  var Playlist = {"My Bloody Valentine":"Sometimes","Phil Ochs":"Here's to the State of Mississippi" }
+  Playlist = {"My Bloody Valentine":"Sometimes","Phil Ochs":"Here's to the State of Mississippi" }
  return playlist 
 }
 
