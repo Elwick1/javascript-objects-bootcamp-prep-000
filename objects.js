c3fb4e2f543c24c82cb33640c6b8b7c3e25ce31d
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-17 13:23:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,7 @@
 var playlist = {"My Bloody Valentine":"Sometimes","Slowdive":"Alison"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-    (playlist['Slowdive'] = 'Alison'
-      playlist['My Bloody Valentine'] = 'Sometimes'
-    })
+    
   return playlist
 }
 
